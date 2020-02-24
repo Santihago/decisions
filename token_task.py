c067581ff37f6070eed71ed17495075c568ebfa6
Santiago Muñoz <santimz@gmail.com>
2020-02-24 17:46:06 +0000

    first visualisation of circles

--- token_task.py
+++ token_task.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 """
@@ -16,8 +16,8 @@ santimz@gmail.com
 
 from psychopy import monitors, gui, visual, core, data, event, logging
 import numpy as np
-#from numpy.random import random, randint, normal, uniform
-#from random import shuffle
+from numpy.random import random, randint, normal, uniform
+from random import shuffle
 #from math import ceil, floor
 import glob, os
 
@@ -35,7 +35,7 @@ myDlg.addField(label='participant',initial=0, tip='Participant name or code'),
 myDlg.addField(label='age',initial=0, tip='Participant age'),
 myDlg.addField(label='sex', choices=('female','male', 'Other/Prefer no to say'))
 myDlg.addField(label='screen', choices=(60, 144), tip='Refresh Rate')
-myDlg.addField(label='triggers', initial='Yes', choices=['Yes', 'No'], tip='Send EEG Triggers')
+myDlg.addField(label='triggers', initial='No', choices=['Yes', 'No'], tip='Send EEG Triggers')
 dlg_data = myDlg.show()
 if myDlg.OK==False: core.quit()  #user pressed cancel
 
@@ -66,28 +66,28 @@ logging.console.setLevel(logging.WARNING)  #this outputs to the screen, not a fi
 #====================================================================
 
 #determine screen's refresh rate
-hz = int(expInfo['screen'])
+# hz = int(expInfo['screen'])
 
-#experiment time durations in seconds
-times= {"REST" : 1., 
-        "CS" : 4., 
-        "US" : 0.5, 
-        "POST_RATING" : 3., 
-        "RATING_MAX" : 10.,
-        "ITI_MIN" : 7., 
-        "ITI_MAX" : 11.}
+# #experiment time durations in seconds
+# times= {"REST" : 1., 
+#         "CS" : 4., 
+#         "US" : 0.5, 
+#         "POST_RATING" : 3., 
+#         "RATING_MAX" : 10.,
+#         "ITI_MIN" : 7., 
+#         "ITI_MAX" : 11.}
 
-#adjust times in seconds to frames depending on screen's refresh rate
-#choose 'ceil' for rounding up, 'floor' for rounding down (no half frames)
-timeInFrames= {  # choose 'ceil' for rounding up, 'floor' for rounding down
-        "REST" : int(ceil(hz * times["REST"])),
-        "CS" : int(ceil(hz * times["CS"])),
-        "US" : int(ceil(hz * times["US"])),
-        "POST_RATING" : int(ceil(hz * times["POST_RATING"])),
-        "RATING_MAX" : int(ceil(hz * times["RATING_MAX"])),
-        "ITI_MIN" : int(ceil(hz * times["ITI_MIN"])),
-        "ITI_MAX" : int(ceil(hz * times["ITI_MAX"]))
-        }
+# #adjust times in seconds to frames depending on screen's refresh rate
+# #choose 'ceil' for rounding up, 'floor' for rounding down (no half frames)
+# timeInFrames= {  # choose 'ceil' for rounding up, 'floor' for rounding down
+#         "REST" : int(ceil(hz * times["REST"])),
+#         "CS" : int(ceil(hz * times["CS"])),
+#         "US" : int(ceil(hz * times["US"])),
+#         "POST_RATING" : int(ceil(hz * times["POST_RATING"])),
+#         "RATING_MAX" : int(ceil(hz * times["RATING_MAX"])),
+#         "ITI_MIN" : int(ceil(hz * times["ITI_MIN"])),
+#         "ITI_MAX" : int(ceil(hz * times["ITI_MAX"]))
+#         }
 
 #=====================
 # Open parallel port
@@ -134,30 +134,35 @@ if triggers:
 # Monitor and screen
 #-------------------
 
-# Monitor ('iiyama 144 Hz')
-widthPix = 1920  #screen width in px
-heightPix = 1080  #screen height in px
-monitorwidth = 53.1  #monitor width in cm
+# Monitor ('Macbook Pro 15')
+widthPix = 2880  #screen width in px
+heightPix = 1800  #screen height in px
+monitorwidth = 33  #monitor width in cm
 viewdist = 60.  #viewing distance in cm
-monitorname = 'iiyama'
+monitorname = 'test'
 scrn = 0  #0 to use main screen, 1 to use external screen
 mon = monitors.Monitor(monitorname, width=monitorwidth, distance=viewdist)
 mon.setSizePix((widthPix, heightPix))
 mon.save()
 
 #Initialize window
-win = visual.Window(
-    monitor=mon,
-    size=(widthPix,heightPix),
-    color=[0,0,0],  #'#B9B9B9' in hex
-    #color=[0,0,.7255],  #'#B9B9B9' in hex
-    colorSpace='hsv',  # !
-    units='deg',
-    screen=scrn,
-    allowGUI=False,
-    fullscr=True)
+# win = visual.Window(
+#     monitor=mon,
+#     size=(widthPix,heightPix),
+#     color=[0,0,0],  #'#B9B9B9' in hex
+#     #color=[0,0,.7255],  #'#B9B9B9' in hex
+#     colorSpace='hsv',  # !
+#     units='deg',
+#     screen=scrn,
+#     allowGUI=False,
+#     fullscr=True)
 
-#win = visual.Window([800, 600], color=[0,0,0], units='deg', colorSpace='hsv', screen=scrn)  # For testing only
+win = visual.Window(monitor=mon,
+                    size=[1200, 800], 
+                    color=[0,0,70], 
+                    units='deg', 
+                    colorSpace='hsv', 
+                    screen=scrn)  # For testing only
 
 #-------
 # MOUSE
@@ -169,76 +174,129 @@ mouse = event.Mouse(visible=True, win=win)
 # VISUAL ELEMENTS
 #-----------------
 
-rest_screen = visual.ImageStim(win=win,
-    image="stim/landscape-1920x1080.jpg", units="deg", size= (32, 18))
+# create 3 big circles
+c_rad = 2 # radius size in dva
+line_color = 'black'
+line_width = 8  # thicker for side circles
 
-# size of the circle radius
-cRad = .3 # size in dva
-# create a big circle
-circle = visual.Circle(win, radius=cRad, lineColor = 'white', fillColor = 'red')
+circle_left  = visual.Circle(win, 
+                             radius=c_rad, 
+                             lineColor=line_color,
+                             lineWidth=line_width, 
+                             pos=(-4,0),
+                             edges=256)
+circle_mid   = visual.Circle(win, 
+                             radius=c_rad, 
+                             lineColor=line_color, 
+                             lineWidth=line_width/2,
+                             pos=(0,0),
+                             edges=256)
+circle_right = visual.Circle(win, 
+                             radius=c_rad, 
+                             lineColor = line_color,
+                             lineWidth=line_width ,
+                             pos=(4,0),
+                             edges=256)
+
+# create list of small circle stimuli
+nr_tokens = 50  # the total number of "numbers" displayed
+token_rad = .15 # in pixels or degrees of visual angle or whatever
+token_color = 'black'
+token_fill  = 'grey'
+token_line_width = .05
+min_pos = -(c_rad) # minimum value for both X and Y axis
+max_pos = c_rad    # maximum value for both X and Y axis
+
+stims = []  #stimuli list
+for this_token in range(nr_tokens):
+    x_pos = uniform(min_pos, max_pos) # set the x pos for this element in particular
+    y_pos = uniform(min_pos, max_pos) # set the y pos for this element in particular
+    stims += [visual.Circle(win, 
+                            radius=token_rad, 
+                            lineColor=token_color,
+                            lineWidth=token_line_width,
+                            fillColor=token_fill,
+                            pos=(x_pos, y_pos))]
 
 #---------------
 # TEXT ELEMENTS
 #---------------
 
-# Instructions text
-instruction_list = []
-text_size = 1  #in degrees of visual angle (dva)
-#fixcross_size = 4  #in degrees of visual angle (dva)
-text_color = 'white'
-text_font = 'arial'
+# # Instructions text
+# instruction_list = []
+# text_size = 1  #in degrees of visual angle (dva)
+# #fixcross_size = 4  #in degrees of visual angle (dva)
+# text_color = 'white'
+# text_font = 'arial'
 
-Merci = visual.TextStim(win,
-    text=u"Merci!",
-    color=text_color ,
-    height=text_size,
-    font=text_font)
+# Merci = visual.TextStim(win,
+#     text=u"Merci!",
+#     color=text_color ,
+#     height=text_size,
+#     font=text_font)
 
 
 #============================================
 # Start main loop that goes through all trials
 #============================================
 
-for this_trial in trials:
+#for this_trial in trials:
 
-## # optionally can perform additional synchronization     
-## ns.sync() 
-ns.send_event('evt_', 
-              label="event", 
-              timestamp=egi.ms_localtime(), 
-              table = {'fld1' : 123, 
-                       'fld2' : "abc", 
-                       'fld3' : 0.042}) 
+for this_trial in range(3):
 
+    #draw big circles
+    circle_left.draw()
+    circle_mid.draw()
+    circle_right.draw()
+
+    #draw all elements
+    for stim in stims:
+        stim.draw()
+    win.flip() # Flip everything
+
+    #Send EEG marker
+    ## # optionally can perform additional synchronization     
+    ## ns.sync() 
+    # if triggers:
+    #     ns.send_event('evt_', 
+    #                 label="event", 
+    #                 timestamp=egi.ms_localtime(), 
+    #                 table = {'fld1' : 123, 
+    #                         'fld2' : "abc", 
+    #                         'fld3' : 0.042}) 
+
+    # Wait for a response
+
+    core.wait(3)
 
 
 #================
 # Save and close
 #================
 
-# Save response data
-trials.saveAsWideText(filename+'.csv')
+# # Save response data
+# trials.saveAsWideText(filename+'.csv')
 
-# Stop  EEG recording
-if triggers:
+# # Stop  EEG recording
+# if triggers:
 
-    ns.send_event('stop')
+#     ns.send_event('stop')
 
-    time.sleep(5)
+#     time.sleep(5)
 
-    ns.StopRecording()
+#     ns.StopRecording()
 
-    ns.EndSession()     
-    ns.disconnect()
+#     ns.EndSession()     
+#     ns.disconnect()
 
 #=======
 # Close
 #=======
 
-# Show 'Merci!' message
-Merci.draw()
-win.flip()
-core.wait(2)
+# # Show 'Merci!' message
+# Merci.draw()
+# win.flip()
+# core.wait(2)
 
 win.close()
-core.quit(
+core.quit()
